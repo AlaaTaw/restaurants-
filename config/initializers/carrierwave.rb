410a47cfa42136345e0e8d8c78cd3be0e957832b
Alaa <alaa.tawalbeh@gmail.com>
2016-04-20 02:00:56 +0300

    6th Add images with carrierwave, S3, and Figaro

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => ENV["AKIAIHSACIYATOHZDIFQ"]
+    :aws_access_key_id      => ENV["AKIAIHSACIYATOHZDIFQ"],
     :aws_secret_access_key  => ENV["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"], 
 }
   config.fog_directory  = ENV["fog_directory"] 
