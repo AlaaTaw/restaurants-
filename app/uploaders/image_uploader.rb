09ba75203b4d7514340a023a56ce7a6fe5f56b5f
Alaa <alaa.tawalbeh@gmail.com>
2016-04-21 23:34:48 +0300

    added fog-aws

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -10,7 +10,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
  if Rails.env.production?
-    storage :fog
+   storage :fog
+end
   else
     storage :file
   end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,14 @@
 CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAIIM366O5JIAI4VEA',
-    :aws_secret_access_key  => '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',
-}
-  config.fog_directory  = 'waibdeh-resturant'
+    provider:              'AWS',                        # required
+    aws_access_key_id:     'AKIAIIM366O5JIAI4VEA',                        # required
+    aws_secret_access_key: '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',                        # required
+   # region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
+ #   host:                  's3.example.com',             # optional, defaults to nil
+  #  endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = 'waibdeh-resturant'                          # required
+ # config.fog_public     = false                                        # optional, defaults to true
+  #config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
