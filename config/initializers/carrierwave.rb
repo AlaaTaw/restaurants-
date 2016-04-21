97be3ecb84b17c34d6ffd7970ae66b1921541cab
Alaa <alaa.tawalbeh@gmail.com>
2016-04-21 18:09:53 +0300

    added @outlook

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAIHSACIYATOHZDIFQ',
-    :aws_secret_access_key  => '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', 
+    :aws_access_key_id      => 'AKIAIIM366O5JIAI4VEA',
+    :aws_secret_access_key  => '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', 
 }
-  config.fog_directory  = 'yelpdemoalaataw' 
+  config.fog_directory  = 'waibdeh-resturant' 
 end
