f2aa575e689e8d1f298ebad21b1b400079ed1934
Alaa <alaa.tawalbeh@gmail.com>
2016-04-21 18:35:03 +0300

    added ignore file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
     :aws_access_key_id      => 'AKIAIIM366O5JIAI4VEA',
-    :aws_secret_access_key  => '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', 
+    :aws_secret_access_key  => 'cM1orEBXgErDDYnKWj4AWX1lwpzf0cLfjfm2deyg',
 }
-  config.fog_directory  = 'waibdeh-resturant' 
+  config.fog_directory  = 'waibdeh-resturant'
 end
